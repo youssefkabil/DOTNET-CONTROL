e4aa223b2c92b292e5cccab7b6b0fe390217b47d
youssefkabil <114140852+youssefkabil@users.noreply.github.com>
2025-01-17 18:13:18 +0100

    project init

---
+++ DOTNET-Control/DOTNET-Control/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DOTNET_Control.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOTNET_Control.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DOTNET-Control/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DOTNET_Control.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
