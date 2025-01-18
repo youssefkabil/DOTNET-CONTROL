2e16881166a5dbf361981be606cdade4dacce814
youssefkabil <114140852+youssefkabil@users.noreply.github.com>
2025-01-18 01:55:06 +0100

    cleaning up files

--- DOTNET-Control/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DOTNET_Control.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
